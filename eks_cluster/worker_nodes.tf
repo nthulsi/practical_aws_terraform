a4baeddbd82595253310df9c14707fceb7752aa3
Dhanapal <61935257+dhanapal703278@users.noreply.github.com>
2022-09-08 10:39:15 +0530

    Update worker_nodes.tf

--- eks_cluster/worker_nodes.tf
+++ eks_cluster/worker_nodes.tf
@@ -6,8 +6,8 @@ resource "aws_eks_node_group" "this" {
   subnet_ids      = aws_subnet.private[*].id
 
   scaling_config {
-    desired_size = 1
-    max_size     = 2
+    desired_size = 2
+    max_size     = 5
     min_size     = 1
   }
 
