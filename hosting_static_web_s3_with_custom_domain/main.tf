74ba4be819796d1b645b7ea1753b8edef2f833e7
dhanapal <dhanapal703278@gmail.com>
2022-09-09 22:53:45 +0530

    new chanages

--- hosting_static_web_s3_with_custom_domain/main.tf
+++ hosting_static_web_s3_with_custom_domain/main.tf
@@ -1,13 +1,23 @@
-resource "aws_s3_bucket" "b" {
-  bucket = "dhanapal-406"
+resource "aws_s3_bucket" "bucket" {
+  bucket = "awsb22.xyz"
    tags = {
     Name        = "My bucket"
     Environment = "Dev"
   }
 }
-
-resource "aws_s3_bucket_acl" "example" {
-  bucket = aws_s3_bucket.b.id
+resource "aws_s3_bucket" "bucket-1" {
+  bucket = "www.awsb22.xyz"
+   tags = {
+    Name        = "My bucket-1"
+    Environment = "Dev-1"
+  }
+}
+resource "aws_s3_bucket_acl" "acl" {
+  bucket = aws_s3_bucket.bucket.id
+  acl    = "private"
+}
+resource "aws_s3_bucket_acl" "acl-1" {
+  bucket = aws_s3_bucket.bucket-1.id
   acl    = "private"
 }
 
