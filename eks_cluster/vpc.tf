4727a9ba59a43f1e585e6284ed1c4b6dcf0998f8
dhanapal <dhanapal703278@gmail.com>
2022-09-16 11:10:11 +0530

    add new chanages in s3-bucket

--- eks_cluster/vpc.tf
+++ eks_cluster/vpc.tf
@@ -102,6 +102,29 @@ resource "aws_route" "main" {
   nat_gateway_id         = aws_nat_gateway.main.id
   destination_cidr_block = "0.0.0.0/0"
 }
+==============
+# Route Table(s)
+# Route the public subnet traffic through the IGW
+resource "aws_route_table" "main" {
+  vpc_id = aws_vpc.this.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.this.id
+  }
+
+  tags = {
+    Name = "${var.project}-Default-rt"
+  }
+}
+
+# Route table and subnet associations
+resource "aws_route_table_association" "internet_access" {
+  count = var.availability_zones_count
+
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.main.id
+}
 
 # Security group for public subnet
 resource "aws_security_group" "public_sg" {
